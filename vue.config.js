41e3c1947c01919eba06f2e69fb84de0e050540b
dearluhj <76814789+dearluhj@users.noreply.github.com>
2021-07-13 13:40:44 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -24,7 +24,7 @@ module.exports = {
     proxy: {
       '/api': {
         // 此处的写法，目的是为了 将 /api 替换成 http://127.0.0.1/
-        target: 'http://127.0.0.1',
+        target: 'http://www.dearlu.top',
         // 允许跨域
         changeOrigin: true,
         ws: true,
